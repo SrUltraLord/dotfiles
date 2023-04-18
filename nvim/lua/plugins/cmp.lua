a3493e296832003bd5374273daaaba8688916762
SrUltraLord <davidrey8822@gmail.com>
2023-04-18 12:48:11 -0500

    fix: file formatting.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -27,61 +27,74 @@ local icons = {
 }
 
 return {
-  "hrsh7th/nvim-cmp",
-  config = function()
-    local cmp = require("cmp")
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = {
-          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-          cmp.config.window.bordered(),
-        },
-        icons = icons
-      },
-      formatting = {
-        format = function(entry, vim_item)
-          vim_item.kind = string.format('%s %s', icons[vim_item.kind], vim_item.kind)
-
-          vim_item.menu = ({
-            buffer = "[Buffer]",
-            nvim_lsp = "[LSP]",
-            luasnip = "[LuaSnip]",
-            nvim_lua = "[Lua]",
-            latex_symbols = "[LaTeX]",
-          })[entry.source.name]
-
-          return vim_item
-        end
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-i>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<cr>'] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = {
+      opts.sources = {
         { name = 'tsserver' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'buffer' },
         { name = 'tailwindcss' },
-        { name = 'tailwindcss' },
         { name = 'astro' },
         { name = 'eslint' },
         { name = 'html' },
+        { name = 'inteliphense' },
         { name = 'rust_analyzer' },
         { name = 'json_ls' },
         { name = 'emmet_ls' },
         { name = 'cssls' }
       }
-    })
-  end,
+    end,
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+            cmp.config.window.bordered(),
+          }
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            -- Kind icons
+            print(vim_item.kind)
+            vim_item.kind = string.format('%s %s', icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            -- Source
+            vim_item.menu = ({
+              buffer = "[Buffer]",
+              nvim_lsp = "[LSP]",
+              luasnip = "[LuaSnip]",
+              nvim_lua = "[Lua]",
+              latex_symbols = "[LaTeX]",
+            })[entry.source.name]
+            return vim_item
+          end
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-i>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<cr>'] = cmp.mapping.confirm({ select = true }),
+        }),
+      })
+    end,
+  },
+
+  -- Autocompletion
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-nvim-lua" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "saadparwaiz1/cmp_luasnip" },
 }
